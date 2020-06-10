2d853c487de1aed04ac1c336281d53e5520cfcb7
alex kozlov <alexkozlov71@gmail.com>
2020-06-10 22:42:45 +0300

    initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+    private String name;
+    private String gender;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+}
---
+++ src/main/java/nameListGradleExample.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class nameListGradleExample {
+    public static List<Person> people = new LinkedList<>();
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int input;
+        boolean processInput = true;
+
+        people.add(new Person("Alex", "M", 15));
+        people.add(new Person("Helen", "F", 25));
+        people.add(new Person("Nikita", "F", 45));
+        people.add(new Person("Trisha", "F", 35));
+        people.add(new Person("Paul", "M", 12));
+        people.add(new Person("Olga", "F", 14));
+        people.add(new Person("Tracy", "F", 17));
+        people.add(new Person("Alex", "M", 10));
+
+        while (processInput) {
+            printMenu();
+            input = scanner.nextInt();
+            switch (input) {
+                case 1:
+                    addPerson();
+                    break;
+                case 2:
+                    people.stream()
+                            .sorted(Comparator.comparing(Person::getAge))
+                            .forEach(System.out::println);
+                    break;
+                case 3:
+                    people.stream()
+                            .filter(person -> "F".equals(person.getGender()))
+                            .forEach(System.out::println);
+                    break;
+                case 0:
+                    System.out.println("See Ya");
+                    processInput = false;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+        }
+        System.exit(0);
+    }
+
+    public static void printMenu() {
+        System.out.println("---------------------------------");
+        System.out.println("-   Main Menu                   -");
+        System.out.println("-       1. Add a person         -");
+        System.out.println("-       2. List people by age   -");
+        System.out.println("-       3. List Females         -");
+        System.out.println("-   0. Exit                     -");
+    }
+
+    public static void addPerson() {
+        System.out.println("Enter Name:");
+        var name = scanner.next();
+        System.out.println("Age:");
+        int age = scanner.nextInt();
+        System.out.println("Gender  M|F:");
+        String gender = scanner.next();
+
+        people.add(new Person(name, gender, age));
+    }
+}
