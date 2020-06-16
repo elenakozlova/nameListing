2371e580ec74381a2f4b8738ecd2c306fe8cdc50
alex kozlov <alexkozlov71@gmail.com>
2020-06-16 10:49:31 +0300

    added test

--- src/test/java/nameListGradleExampleTest.java
+++ src/test/java/nameListGradleExampleTest.java
@@ -8,7 +8,9 @@ import java.util.stream.Stream;
 
 public class nameListGradleExampleTest extends TestCase {
     public void testAddDefaultUsers() {
-        System.out.println("Testing number of users");
+        System.out.println("-------------------------------------");
+        System.out.println("Testing number of people on the list");
+        System.out.println("-------------------------------------");
         nameListGradleExample.addDefaultUsers();
         List<Person> people = nameListGradleExample.people;
         assertEquals(8, people.size());
