2f923fc5ee3d3a284479d1ed678bb68e9f64d679
elena.kozlova <elena.kozlova@jetbrains.com>
2020-07-02 18:47:16 +0300

    changes

--- src/main/java/nameListGradleExample.java
+++ src/main/java/nameListGradleExample.java
@@ -1,31 +1,19 @@
-
 import java.util.*;
 
+import static java.util.logging.Logger.getLogger;
+
 public class nameListGradleExample {
-  public static List<Person> people = new LinkedList<>();
+  public static List <Person> people = new LinkedList<>();
+  public static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-
     int input;
     boolean processInput = true;
     addDefaultUsers();
-
-
-
+    print();
     while (processInput) {
       printMenu();
-      System.out.println("aaaa");
-      try {
-
-        var in = scanner.next();
-      } catch (NoSuchElementException e) {
-        e.printStackTrace();
-      }
-
-//      System.out.println(/ /in);
-//      input = Integer.parseInt(in);
-
+      input = scanner.nextInt();
       switch (input) {
         case 1:
           addPerson();
@@ -39,11 +27,14 @@ public class nameListGradleExample {
           people.stream()
                   .filter(person -> "F".equals(person.getGender()))
                   .forEach(System.out::println);
+          long count = people.stream()
+                  .filter(person -> "F".equals(person.getGender()))
+                  .count();
+          System.out.printf("total number of Females %,d \n", count);
           break;
         case 0:
           System.out.println("See Ya");
           processInput = false;
-          break;
         default:
           System.out.println("Invalid input");
           break;
@@ -53,6 +44,7 @@ public class nameListGradleExample {
   }
 
   public static void addDefaultUsers() {
+    new Thread(()-> System.out.println("test lambda"))   ;
     people.add(new Person("Alex", "M", 15));
     people.add(new Person("Helen", "F", 25));
     people.add(new Person("Nikita", "F", 45));
@@ -61,25 +53,26 @@ public class nameListGradleExample {
     people.add(new Person("Olga", "F", 14));
     people.add(new Person("Tracy", "F", 17));
     people.add(new Person("Alex", "M", 10));
-    //people.add(new Person("Shakira", "F", 35));
   }
 
   public static void printMenu() {
     System.out.println("---------------------------------");
-    System.out.println("-   Main Menu                   -");
-    System.out.println("-       1. Add a person         -");
-    System.out.println("-       2. List people by age   -");
-    System.out.println("-       3. List Females         -");
-    System.out.println("-   0. Exit                     -");
+    System.out.println("- Main Menu -");
+    System.out.println("- 1. Add a person -");
+    System.out.println("- 2. List people by age -");
+    System.out.println("- 3. List Females -");
+    System.out.println("- 0. Exit -");
+  }
+  public static void  print(){
+    System.out.println("Hello,World!");
   }
 
   public static void addPerson() {
-    Scanner scanner = new Scanner(System.in);
     System.out.println("Enter Name:");
     String name = scanner.next();
     System.out.println("Age:");
     int age = scanner.nextInt();
-    System.out.println("Gender  M|F:");
+    System.out.println("Gender M|F:");
     String gender = scanner.next();
 
     people.add(new Person(name, gender, age));
